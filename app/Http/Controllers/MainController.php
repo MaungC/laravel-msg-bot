12c60a5beeb16149064bfb7ff300522a676a8da0
CC <helkkaunglay@gmail.com>
2019-01-22 23:33:45 +0630

    new cmt

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,7 +13,7 @@ class MainController extends Controller
         
             //get the user’s id
             $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-        $this->sendTextMessage($id, "Hello");
+        $this->sendTextMessage($id, $data["entry"][0]["messaging"][0]);
     }
 
 private function sendTextMessage($recipientId, $messageText)
