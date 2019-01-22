db1751e5688a2afb5d9473d14abd4e3e88892d96
CC <helkkaunglay@gmail.com>
2019-01-22 23:37:34 +0630

    new cmt

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,11 +9,10 @@ class MainController extends Controller
     public function receive(Request $request)
     {
             $data = $request->all();
-            dd($data);
         
             //get the user’s id
             $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-        $this->sendTextMessage($id, "Hello friend");
+        $this->sendTextMessage($id, "Hello");
     }
 
 private function sendTextMessage($recipientId, $messageText)
