8144b80236a9f6e0c2f4145663cba00f7868f8b5
CC <helkkaunglay@gmail.com>
2019-01-22 23:44:45 +0630

    new cmt

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,7 +13,7 @@ class MainController extends Controller
             //get the user’s id
             $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
             dd($id);
-        $this->sendTextMessage($id, "Hello friend, Welcom my 'Chat Bot' !. What can i help?");
+        $this->sendTextMessage($id, "Hello friend, Welcom my Chat Bot. What can i help?");
     }
 
 private function sendTextMessage($recipientId, $messageText)
